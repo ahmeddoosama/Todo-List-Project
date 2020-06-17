69023ae06b6632a2419756a3cbdc36b58d7c9260
Ahmed Osama <ahmedosamabedawy@gmail.com>
2020-06-17 22:43:01 +0200

    Add Function DeleteItem

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AddItem from './Component/AddItem/index'
 import TodoItems from './Component/TodoItems/index'
@@ -6,16 +6,36 @@ import RecycleBen from './Component/TodoRecycleBen/index'
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Todo-List Project
+class App extends Component {
 
-      <AddItem />
-      <TodoItems />
-      <RecycleBen />
-    </div>
-  );
+  state = {
+    items : [
+      {id: 1, name: 'Ahmed Osama'},
+      {id: 2, name: 'Mohamed Osama'},
+      {id: 3, name: 'Ali Osama'}
+    ]
+  }
+
+
+  deleteItem = (id) => {
+    let allItems = this.state.items.filter(item => {
+      return item.id != id
+    })
+
+    this.setState({items: allItems})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Todo-List Project
+  
+        <AddItem />
+        <TodoItems showItems={this.state.items} deleteItem={this.deleteItem} />
+        <RecycleBen />
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-js/src/Component/TodoItems/index.js
+++ react-js/src/Component/TodoItems/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './style.css'
 
-const TodoItems = () => {
+const TodoItems = (props) => {
+
+    const {showItems, deleteItem} = props
+    const ListItems = showItems.map(item => {
+        return (
+            <div key={item.id}>
+                <span>{item.name}</span>
+                <span onClick={() => deleteItem(item.id)}> &times; </span>
+            </div>
+        )
+    })
+
+
     return (
         <div>
-            List Items
+            {ListItems}
         </div>
     )
 }
